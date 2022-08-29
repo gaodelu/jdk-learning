a7eebc7050160484c44f03c897748584b01d2af7
gaodelu <gaodelu0752@163.com>
2022-08-29 17:57:31 +0800

    jdk12内容更新

---
+++ src/com/mochi/jdk12/README.md
@@ -0,0 +1,17 @@
+# jdk12新特性
+## 新功能和库的更新
+### switch表达式新特性：支持lambda表达式
+
+### NumberFormat增加了对以紧凑格式格式化数字的支持
+
+### Files中新增方法mismatch()
+
+### String功能新增
+- indent() 添加空格
+- transform() 链式转换
+
+### JVM常量api
+
+## JVM优化
+### 雪兰多收集器
+
--- src/com/mochi/jdk12/StringTest.java
+++ src/com/mochi/jdk12/StringTest.java
@@ -4,7 +4,6 @@ public class StringTest {
 
     public static void main(String[] args) {
         //对字符串进行链式转换
-        Integer a=null;
         String transform = "abc".transform(x->x+"def").transform(String::toUpperCase);
         System.out.println(transform);
         //在字符串的每一行前面加空格
